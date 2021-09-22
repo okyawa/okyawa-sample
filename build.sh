b0256bad7ab7ab8c93c472e4bcb4342095b3345e
Hirokazu Okawa <h.okyawa@gmail.com>
2021-09-22 16:12:01 +0900

    Teachable MachineのAudio Modelを試す

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ mkdir -p ./public/barcode-scanner
 cp -r ./barcode-scanner/ ./public/barcode-scanner/
 
 # teachable-machine/pose-model
-mkdir -p ./public/teachable-machine/pose-model
-cp -r ./teachable-machine/pose-model/ ./public/teachable-machine/pose-model/
+mkdir -p ./public/teachable-machine
+cp -r ./teachable-machine/ ./public/teachable-machine/
 
