082196759ac9d5e5da31c1bdd0682da22afa9213
Hirokazu Okawa <h.okyawa@gmail.com>
2023-07-19 16:43:41 +0900

    add: セレクトボックスだけHTMLに設置で、JSでinput type="date"を生成するクラスを使うパターンを追加

---
+++ html-tips/input-date/js/select-date-calender.js
@@ -0,0 +1,189 @@
+/**
+ * SelectDateCalendarのオプションobjectの型
+ * @typedef {object} SelectDateCalendarOptionsType
+ * @property {string} namePrefix 生成するlabel要素に付与するクラス名
+ * @property {string} labelClassName 生成する input type="date" 要素のname属性値のプレフィックス
+ */
+
+/**
+ * セレクトボックスとinput type="date"のカレンダーを連動させるクラス
+ */
+class SelectDateCalendar {
+  /**
+   * コンストラクター
+   * @param {HTMLSelectElement} selectElement 日付選択セレクトボックスの要素
+   * @param {SelectDateCalendarOptionsType} params オプション指定のオブジェクト
+   */
+  constructor(selectElement, params) {
+    /** @type {HTMLSelectElement} */
+    this.selectElement = selectElement;
+    /** @type {SelectDateCalendarOptionsType} */
+    this.options = { ...SelectDateCalendar.defaults, ...params };
+    /** @type {HTMLInputElement} */
+    this.inputDateElement = this.createInputDateElement();
+    this.appendInputDateElement();
+  }
+
+  /**
+   * オプションの初期値
+   * @type {SelectDateCalendarOptionsType}
+   */
+  static defaults = {
+    namePrefix: 'calendar_',
+    labelClassName: 'date_edit_label',
+  }
+
+  /**
+   * 初期化
+   */
+  init() {
+    this.setupEvent();
+    this.syncInputDateElem(this.selectElement.value);
+  }
+
+  /**
+   * input type="date"の要素を生成
+   * @return {HTMLInputElement} input type="date"の要素
+   * @private
+   */
+  createInputDateElement() {
+    const inputDateElement = document.createElement('input');
+    inputDateElement.setAttribute('type', 'date');
+    const name =
+      this.options.namePrefix + this.selectElement.getAttribute('name');
+    inputDateElement.setAttribute('name', name);
+    // TODO: セレクトボックスのoptionの最初と最後を取得し、min と max の属性を指定
+    return inputDateElement;
+  }
+
+  /**
+   * セレクトボックスの後ろに input type="date" の要素を追加
+   * @private
+   */
+  appendInputDateElement() {
+    // ラベル要素
+    const labelElement = document.createElement('label');
+    labelElement.classList.add(this.options.labelClassName);
+    // カレンダーアイコン
+    const iElement = document.createElement('i');
+    iElement.classList.add('fa', 'fa-calendar');
+    iElement.setAttribute('aria-hidden', 'true');
+    // DOMに追加
+    labelElement.appendChild(this.inputDateElement);
+    labelElement.appendChild(iElement);
+    this.selectElement.parentNode.insertBefore(
+      labelElement,
+      this.selectElement.nextSibling
+    );
+  }
+
+  /**
+   * イベントの初期化
+   * @private
+   */
+  setupEvent() {
+    this.inputDateElement.addEventListener('click', this.clickCalendarHandler.bind(this));
+    this.inputDateElement.addEventListener('input', this.syncSelectElem.bind(this));
+    this.selectElement.addEventListener('input', this.selectBoxInputHandler.bind(this));
+  }
+
+  /**
+   * カレンダーアイコンをクリックしたときのハンドラー
+   * @param {MouseEvent} event 
+   * @private
+   */
+  clickCalendarHandler(event) {
+    /** @type HTMLInputElement | null */
+    const targetElem = event.currentTarget;
+    if (targetElem === null) {
+      return;
+    }
+    if (targetElem.showPicker === undefined) {
+      console.log('Note: This browser is not supported showPicker() method.');
+    }
+    targetElem.showPicker();
+  }
+
+  /**
+   * 日付選択セレクトボックスのinputイベントのハンドラー
+   * @param {InputEvent} event 日付選択セレクトボックスのinputイベント
+   * @private
+   */
+  selectBoxInputHandler(event) {
+    /** @type {HTMLSelectElement | null} */
+    const targetElem = event.currentTarget;
+    if (targetElem === null) {
+      return;
+    }
+    const dateValue = targetElem.value;
+    this.syncInputDateElem(dateValue);
+  }
+
+  /**
+   * セレクトボックスの値を input type="date" の要素の値に同期させる
+   * @param {string} dateValue 日付選択セレクトボックスの選択値
+   * @private
+   */
+  syncInputDateElem(dateValue) {
+    if (dateValue === undefined) {
+      this.inputDateElement.value = '';
+      return;
+    }
+    this.inputDateElement.value = dateValue;
+    if (this.inputDateElement.value === '') {
+      // リストアップしていないものをセットした場合、未選択のラベルが表示されるようにする
+      this.inputDateElement.value = '';
+    }
+  }
+
+  /**
+   * input type="date" の値を日付選択セレクトボックスの値に同期させる
+   * @param {InputEvent} event input type="date" のinputイベント
+   * @private
+   */
+  syncSelectElem(event) {
+    /** @type {HTMLInputElement | null} */
+    const targetElem = event.currentTarget;
+    if (targetElem === null) {
+      return;
+    }
+    const dateValue = targetElem.value;
+    if (dateValue === undefined) {
+      this.selectElement.value = '';
+      return;
+    }
+    this.selectElement.value = dateValue;
+  }
+}
+
+/**
+ * SelectDateCalendarの初期化
+ * @param {string} selector 対象の日付選択セレクトボックスのセレクター
+ * @param {object} params オプション指定のオブジェクト
+ */
+function setupSelectDateCalendar(selectElement, params) {
+  if (!selectElement instanceof HTMLSelectElement) {
+    console.error('Error: setupSelectDateCalendar() - "selectElement" is not instanceof HTMLSelectElement.')
+    return;
+  }
+  const selectDateCalendar = new SelectDateCalendar(selectElement, params);
+  selectDateCalendar.init();
+}
+
+/**
+ * 対象のセレクトボックス全てにSelectDateCalendarを初期化
+ * @param {string} selector 対象の日付選択セレクトボックスのセレクター
+ * @param {object} params オプション指定のオブジェクト
+ */
+function setupSelectDateCalendarAll(selector, params) {
+  const selectElements = document.querySelectorAll(selector);
+  selectElements.forEach((selectElement) => {
+    setupSelectDateCalendar(selectElement, params);
+  });
+}
+
+// TODO: input type="date" のmixとmaxが効かないブラウザ(safari)の場合、範囲外の日付を選択した場合にエラー文を表示
+
+window.SelectDateCalendar = SelectDateCalendar;
+window.setupSelectDateCalendar = setupSelectDateCalendar;
+window.setupSelectDateCalendarAll = setupSelectDateCalendarAll;
