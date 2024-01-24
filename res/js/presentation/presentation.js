2879cbcf04d81805176dc4acb2cb97e92e8b2597
Hirokazu Okawa <h.okyawa@gmail.com>
2024-01-24 18:31:21 +0900

    refactor: プレゼンテーション用のCSSとJSを外部化

---
+++ res/js/presentation/presentation.js
@@ -0,0 +1,20 @@
+/**
+ * プレゼンテーション用にアコーディオンの動作となるモードにする
+ */
+function switchPresentationMode() {
+  const detailsElements = document.querySelectorAll('details');
+  detailsElements.forEach((detailsElement) => {
+    // detailsの中で1つだけを開く指定
+    detailsElement.name = 'section';
+  });
+}
+
+/**
+ * 全てのdetails要素を閉じる
+ */
+function closeAllDetails() {
+  const detailsElements = document.querySelectorAll('details');
+  detailsElements.forEach((detailsElement) => {
+    detailsElement.open = false;
+  });
+}
