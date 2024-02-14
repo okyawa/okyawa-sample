5bc3761ce34880d9335e0b0514fd7dc490ac806e
Hirokazu Okawa <h.okyawa@gmail.com>
2024-02-14 15:52:50 +0900

    refactor: JSでのTS変換処理を外部ファイル化

---
+++ js-tips/typescript-on-web/ts-transpile.js
@@ -0,0 +1,27 @@
+// TypeScript本家のパッケージ
+import { transpile, ScriptTarget } from 'https://esm.sh/typescript@5.3.3?bundle&target=esnext';
+// ソースコード圧縮し、minifyするパッケージ
+import { minify_sync } from 'https://esm.sh/terser@5.27.0?bundle&target=esnext';
+
+/**
+ * TypeScriptファイルを読み込み、JavaScriptにトランスパイルして圧縮したDataURLを返す
+ * @param {string} url TypeScriptファイルのURL
+ * @returns {Promise<string>}
+ */
+export async function transpileTS(url) {
+  const response = await fetch(url);
+  const tsText = await response.text();
+
+  // TypeScriptからJavaScriptへのトラインスパイルを実行
+  const js = transpile(tsText, {
+    target: ScriptTarget.ESNext,
+  });
+
+  // ソースコードの圧縮
+  const { code } = minify_sync(js, {
+    module: true,
+  });
+
+  // UTF-8でエンコードしBASE64へ変換したDataURLへ変換
+  return `data:text/javascript;base64,${btoa([...new TextEncoder().encode(code)].map(v => String.fromCharCode(v)).join(''))}`;
+}
