69991d5b95566d799f2e4a3d4724b5495cd4d843
Hirokazu Okawa <h.okyawa@gmail.com>
2024-07-05 11:06:32 +0900

    refactor: DialogImage.init() から関数部分をリファクタリング

--- html-tips/dialog-image/dialog-image.js
+++ html-tips/dialog-image/dialog-image.js
@@ -9,11 +9,14 @@ class DialogImage {
   constructor(config) {
     /** @type {DialogImageOptionType} オプション */
     this.options = { ...this.defaults, ...config };
+    /** @type {HTMLDialogElement} 画像拡大表示するdialog要素 */
+    this.modalDialog = document.querySelector(`#${this.options.dialogId}`);
   }
 
   /**
    * オプションの初期値
    * @type {DialogImageOptionType}
+   * @private
    */
   get defaults() {
     // ※PHPStormでツールチップ表示やジャンプが動作するよう、敢えて一旦変数へ格納し、型指定を当てている
@@ -26,12 +29,14 @@ class DialogImage {
     return values;
   }
 
+  /**
+   * 初期化処理
+   */
   init() {
-    const modalDialog = document.querySelector(`#${this.options.dialogId}`);
     const openLinkElements = typeof this.options.openLink === 'string'
       ? document.querySelectorAll(this.options.openLink)
       : this.options.openLink;
-    if (!(modalDialog instanceof HTMLDialogElement)) {
+    if (!(this.modalDialog instanceof HTMLDialogElement)) {
       // dialog要素ではない場合は中断
       return;
     }
@@ -46,79 +51,23 @@ class DialogImage {
         const targetElem = event.currentTarget;
         const url = targetElem.getAttribute('href');
         const caption = targetElem.dataset.caption ?? '';
-        openImagePreviewDialog(url, caption);
+        this.openImagePreviewDialog(url, caption);
       });
     })
 
     /**
-     * dialog要素のアニメーションがすべて終了するのを待つ関数
-     */
-    function waitDialogAnimation(dialog) {
-      return Promise.allSettled(dialog.getAnimations().map((animation) => animation.finished));
-    }
-
-    /**
-     * 画像拡大表示ダイアログを開く
-     */
-    async function openImagePreviewDialog(url, caption) {
-      const imagePreviewElem = modalDialog.querySelector('.image_preview');
-      // 拡大画像表示の処理
-      imagePreviewElem.innerHTML = `<img src="${url}" alt="" />`;
-
-      // キャプションのテキスト
-      const captionElem = modalDialog.querySelector('.image_caption');
-      if (caption) {
-        captionElem.innerHTML = `<div class="caption_text">${caption}</div>`;
-        modalDialog.classList.add('has_caption');
-      } else {
-        captionElem.innerHTML = '';
-        modalDialog.classList.remove('has_caption');
-      }
-
-      // 表示画像自体のクリック
-      modalDialog.querySelector('.image_preview img')?.addEventListener('click', (event) => {
-        if (modalDialog.classList.contains('zoom')) {
-          // ズーム中の場合は縮小表示に戻す
-          modalDialog.classList.remove('zoom');
-          return;
-        }
-        // コントロール表示を切り替え
-        modalDialog.classList.toggle('controls_hidden');
-      });
-
-      // dialog要素の開くアニメーションがすべて終了するまで待つ
-      await waitDialogAnimation(modalDialog);
-
-      // dialog要素にデフォルトで付与していたdisplayのstyle指定を削除
-      modalDialog.style.display = '';
-      // モーダルダイアログを開く
-      modalDialog.showModal();
-      // モーダルダイアログを表示する際に、HTML要素(背景部分)がスクロールしないようにする
-      document.documentElement.style.overflow = 'hidden';
-
-      // 表示する画像に拡大ボタンが必要かを判定
-      const { width, height } = await readImageSize(url);
-      const zoomEnabled = width > imagePreviewElem.clientWidth || height > imagePreviewElem.clientHeight
-      if (zoomEnabled) {
-        modalDialog.classList.remove('zoom_disabled');
-      } else {
-        modalDialog.classList.add('zoom_disabled');
-      }
-    }
-
-    /**
      * ダイアログの枠外をクリックした際にダイアログを閉じる
      */
-    modalDialog.addEventListener('click', (event) => {
+    this.modalDialog.addEventListener('click', (event) => {
       if (event.target === event.currentTarget) {
-        modalDialog.close();
+        this.modalDialog.close();
       }
     });
 
     /**
      * ダイアログを閉じる際に実行するイベントを登録
      */
-    modalDialog.addEventListener('close', async (event) => {
+    this.modalDialog.addEventListener('close', async (event) => {
       const dialog = event.currentTarget;
       if (!(dialog instanceof HTMLDialogElement)) {
         // dialog要素ではない場合は中断
@@ -129,10 +78,10 @@ class DialogImage {
       // dialog要素のstyle指定で非表示にする
       dialog.style.display = 'none';
       // 判定用に付与したクラス名を初期化
-      modalDialog.classList.remove('zoom');
-      modalDialog.classList.remove('zoom_disabled');
-      modalDialog.classList.remove('has_caption');
-      modalDialog.classList.remove('controls_hidden');
+      this.modalDialog.classList.remove('zoom');
+      this.modalDialog.classList.remove('zoom_disabled');
+      this.modalDialog.classList.remove('has_caption');
+      this.modalDialog.classList.remove('controls_hidden');
       // 背景スクロールを防ぐために追加したスタイルを削除
       document.documentElement.style.overflow = '';
     });
@@ -140,40 +89,106 @@ class DialogImage {
     /**
      * 拡大ボタンのイベント登録
      */
-    modalDialog.querySelector('.zoom_in_button')?.addEventListener('click', (event) => {
-      modalDialog.classList.add('zoom');
+    this.modalDialog.querySelector('.zoom_in_button')?.addEventListener('click', (event) => {
+      this.modalDialog.classList.add('zoom');
     });
 
     /**
      * 縮小ボタンのイベント登録
      */
-    modalDialog.querySelector('.zoom_out_button')?.addEventListener('click', (event) => {
-      modalDialog.classList.remove('zoom');
+    this.modalDialog.querySelector('.zoom_out_button')?.addEventListener('click', (event) => {
+      this.modalDialog.classList.remove('zoom');
     });
 
-    /**
-     * 画像ファイルのサイズを取得
-     */
-    async function readImageSize(url) {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
+  }
 
-        img.onload = () => {
-          const size = {
-            width: img.naturalWidth,
-            height: img.naturalHeight,
-          };
+  /**
+   * 画像拡大表示ダイアログを開く
+   */
+  async openImagePreviewDialog(url, caption) {
+    const imagePreviewElem = this.modalDialog.querySelector('.image_preview');
+    // 拡大画像表示の処理
+    imagePreviewElem.innerHTML = `<img src="${url}" alt="" />`;
 
-          URL.revokeObjectURL(img.src);
-          resolve(size);
-        };
+    // キャプションのテキスト
+    const captionElem = this.modalDialog.querySelector('.image_caption');
+    if (caption) {
+      captionElem.innerHTML = `<div class="caption_text">${caption}</div>`;
+      this.modalDialog.classList.add('has_caption');
+    } else {
+      captionElem.innerHTML = '';
+      this.modalDialog.classList.remove('has_caption');
+    }
 
-        img.onerror = (error) => {
-          reject(error);
-        };
+    // 表示画像自体のクリック
+    this.modalDialog.querySelector('.image_preview img')?.addEventListener('click', (event) => {
+      if (this.modalDialog.classList.contains('zoom')) {
+        // ズーム中の場合は縮小表示に戻す
+        this.modalDialog.classList.remove('zoom');
+        return;
+      }
+      // コントロール表示を切り替え
+      this.modalDialog.classList.toggle('controls_hidden');
+    });
 
-        img.src = url;
-      });
+    // dialog要素の開くアニメーションがすべて終了するまで待つ
+    await waitDialogAnimation(this.modalDialog);
+
+    // dialog要素にデフォルトで付与していたdisplayのstyle指定を削除
+    this.modalDialog.style.display = '';
+    // モーダルダイアログを開く
+    this.modalDialog.showModal();
+    // モーダルダイアログを表示する際に、HTML要素(背景部分)がスクロールしないようにする
+    document.documentElement.style.overflow = 'hidden';
+
+    // 表示する画像に拡大ボタンが必要かを判定
+    const { width, height } = await readImageSize(url);
+    const zoomEnabled = width > imagePreviewElem.clientWidth || height > imagePreviewElem.clientHeight
+    if (zoomEnabled) {
+      this.modalDialog.classList.remove('zoom_disabled');
+    } else {
+      this.modalDialog.classList.add('zoom_disabled');
     }
   }
+}
+
+window.DialogImage = DialogImage;
+
+/**
+ * dialog要素のアニメーションがすべて終了するのを待つ関数
+ * @type {HTMLDialogElement} dialog dialog要素
+ * @returns {Promise<PromiseSettledResult<any>[]>}
+ */
+function waitDialogAnimation(dialog) {
+  return Promise.allSettled(
+    dialog.getAnimations()
+      .map((animation) => animation.finished)
+  );
+}
+
+/**
+ * 画像ファイルのサイズを取得
+ * @param {string} url 画像ファイルのURL
+ * @private
+ */
+async function readImageSize(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.onload = () => {
+      const size = {
+        width: img.naturalWidth,
+        height: img.naturalHeight,
+      };
+
+      URL.revokeObjectURL(img.src);
+      resolve(size);
+    };
+
+    img.onerror = (error) => {
+      reject(error);
+    };
+
+    img.src = url;
+  });
 }
